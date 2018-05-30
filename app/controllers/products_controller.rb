b52cdd7dc8e01d3d085903aef1ee7a42dc7cb89e
Rabi Róbert <ingenmaffen@gmail.com>
2018-05-31 01:15:49 +0200

    product detailer design fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id, :images)
+      params.require(:product).permit(:name, :description, :price, :category_id, :images, :stock)
     end
 end
